d5aea7a83a96a8fd8b5dba1c79cf235e6b081fe9
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2023-06-10 19:05:40 +0200

    completed set up scaffold

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,34 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+"use client";
+import type { AppProps } from "next/app";
+import GlobalStyle from "../components/globalstyles";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorComponent from "../components/ErrorComponent/ErrorComponent";
 
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
+const logError = (error: Error, info?: { componentStack: string }) => {
+  // send the errors to sentry
+};
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    // this will log all errors ecncountered during data fetching
+    onError: (error) => {
+      logError(error as unknown as Error);
+    },
+  }),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ThemeProvider theme={theme}>
+    <ErrorBoundary FallbackComponent={ErrorComponent} onError={logError}>
+      <QueryClientProvider client={queryClient}>
         <GlobalStyle />
         <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
 }
